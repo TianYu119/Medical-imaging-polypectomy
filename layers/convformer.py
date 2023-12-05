eef76c84be4ee95690064a2e1ec13490f38d5004
TianYu119 <gfyy2219@163.com>
2023-12-06 01:11:16 +0800

    END

--- layers/convformer.py
+++ layers/convformer.py
@@ -4,11 +4,61 @@ from tensorflow.keras.regularizers import l2
 
 def convformer(input_tensor, filters, padding="same"):
     x = tf.keras.layers.LayerNormalization()(input_tensor)
+    # input_tensor = tf.keras.layers.BatchNormalization()(input_tensor)
     # x = tf.keras.layers.SeparableConv2D(filters, kernel_size=(3, 3), padding=padding)(x)
     x = tf.keras.layers.SeparableConv2D(filters, kernel_size=(3, 3), padding=padding, 
-                                         kernel_regularizer=l2(0.01))(x)  # 添加正则化
+                                         kernel_regularizer=l2(0.01))(input_tensor)  # 添加正则化
+    # x = tf.keras.layers.BatchNormalization()(x)
 
-    # #空间注意力
+        # 空间注意力
+    spatial_attention = SpatialAttentionLayer()(x)
+
+
+    # 在此处加入边界聚合模块
+    merged_with_boundary_s = boundary_aggregation_module(spatial_attention, filters)
+
+    # 通道注意力
+    channel_attention = ChannelAttentionLayer()(x)
+
+    # 在此处加入边界聚合模块
+    merged_with_boundary_c = boundary_aggregation_module(channel_attention, filters)
+
+
+    # 元素级相加合并注意力输出
+    merged_attention = tf.keras.layers.Add()([merged_with_boundary_s, merged_with_boundary_c])
+  
+    # 边界预测
+    boundary_feature = boundary_prediction_subnetwork(merged_attention, filters)
+    
+    # 融合边界特征
+    # 这里只是一个简单的融合示例，您可以使用其他方法如Concatenate，Multiply等
+    fused = tf.keras.layers.Add()([merged_attention, boundary_feature])
+    
+    # MLP层
+    mlp_output = MLPBlock(units=filters)(fused)
+    
+    
+    # # MLP层
+    # mlp_output = MLPBlock(units=filters)(merged_attention)
+
+    # small_feature_conv = tf.keras.layers.Conv2D(filters, kernel_size=(2, 2), padding="same", activation="relu")(input_tensor)   
+    # small_feature_conv = tf.keras.layers.LayerNormalization()(small_feature_conv)
+
+
+    out = tf.keras.layers.Add()([mlp_output, input_tensor])
+    # out = tf.keras.layers.LayerNormalization()(out)
+
+
+
+
+    x1 = tf.keras.layers.Dense(filters, activation="relu")(out)
+    x1 = tf.keras.layers.Dense(filters)(x1)
+    out_tensor = tf.keras.layers.Add()([out, x1])
+
+
+
+
+        # #空间注意力
     # attn_output = SpatialAttentionLayer()(x)
     # x = tf.keras.layers.Add()([x, attn_output])
 
@@ -21,40 +71,96 @@ def convformer(input_tensor, filters, padding="same"):
     # out = tf.keras.layers.Add()([mlp_output, input_tensor])
 
 
+    #     # 空间注意力
+    #     spatial_attention = SpatialAttentionLayer()(x)
+
+    #     # 通道注意力
+    #     channel_attention = ChannelAttentionLayer()(x)
+
+    #     # 元素级相加合并注意力输出
+    #     merged_attention = tf.keras.layers.Add()([spatial_attention, channel_attention,x])
+    #    # x = tf.keras.layers.BatchNormalization()(merged_attention)  # 添加批次归一化
+    #     # MLP层
+    #     mlp_output = MLPBlock(units=filters)(merged_attention)
+    #     out = tf.keras.layers.Add()([mlp_output, input_tensor])
+
+
+    # mlp_output = MLPBlock(units=filters)(x)
     # # 空间注意力
-    # spatial_attention = SpatialAttentionLayer()(x)
+    # spatial_attention = SpatialAttentionLayer()(mlp_output)
 
     # # 通道注意力
-    # channel_attention = ChannelAttentionLayer()(x)
+    # channel_attention = ChannelAttentionLayer()(mlp_output)
+    # merged_attention = tf.keras.layers.Add()([spatial_attention, channel_attention])
+    # merged_attention= tf.keras.layers.BatchNormalization()(merged_attention)  # 添加批次归一化
+    # merged_attention=tf.keras.layers.Add()([merged_attention,mlp_output])
+    # out = tf.keras.layers.Add()([merged_attention, x])
 
-    # # 元素级相加合并注意力输出
-    # merged_attention = tf.keras.layers.Add()([spatial_attention, channel_attention,x])
-    # x = tf.keras.layers.BatchNormalization()(merged_attention)  # 添加批次归一化
-    # # MLP层
-    # mlp_output = MLPBlock(units=filters)(merged_attention)
-    # out = tf.keras.layers.Add()([mlp_output, input_tensor])
+    # small_feature_conv = tf.keras.layers.Conv2D(filters, kernel_size=(3, 3), padding="same", activation="relu")(out)
+
+    # out = tf.keras.layers.Add()([small_feature_conv, out])
+    # # x1 = tf.keras.layers.Dense(filters, activation="swish")(out)
 
 
-    mlp_output = MLPBlock(units=filters)(x)
-    # 空间注意力
-    spatial_attention = SpatialAttentionLayer()(mlp_output)
 
-    # 通道注意力
-    channel_attention = ChannelAttentionLayer()(mlp_output)
-    merged_attention = tf.keras.layers.Add()([spatial_attention, channel_attention])
-    merged_attention= tf.keras.layers.BatchNormalization()(merged_attention)  # 添加批次归一化
-    merged_attention=tf.keras.layers.Add()([merged_attention,mlp_output])
-    out = tf.keras.layers.Add()([merged_attention, input_tensor])
 
-    small_feature_conv = tf.keras.layers.Conv2D(filters, kernel_size=(1, 1), padding="same", activation="relu")(out)
 
-    out = tf.keras.layers.Add()([small_feature_conv, input_tensor])
-    # x1 = tf.keras.layers.Dense(filters, activation="swish")(out)
-    x1 = tf.keras.layers.Dense(filters, activation="relu")(out)
-    x1 = tf.keras.layers.Dense(filters)(x1)
-    out_tensor = tf.keras.layers.Add()([out, x1])
+
 
     return out_tensor
+
+
+
+# def boundary_aggregation_module(input_tensor, filters):
+#     # 使用Sobel滤波器来检测边界
+#     sobel_x = tf.image.sobel_edges(input_tensor)[:,:,:,0]
+#     sobel_y = tf.image.sobel_edges(input_tensor)[:,:,:,1]
+#     sobel_output = tf.math.abs(sobel_x) + tf.math.abs(sobel_y)
+
+#     # 使用Conv层进一步提取边界特征
+#     edge_features = tf.keras.layers.Conv2D(filters, kernel_size=(3,3), padding='same', activation='relu')(sobel_output)
+    
+#     # 使用一个1x1的Conv层来调整通道数量并与原始输入合并
+#     edge_features = tf.keras.layers.Conv2D(filters, kernel_size=(1,1), padding='same', activation='relu')(edge_features)
+
+#     # 合并原始特征和边界特征
+#     merged = tf.keras.layers.Add()([input_tensor, edge_features])
+    
+#     return merged
+
+def boundary_aggregation_module(input_tensor, filters):
+    # 使用Sobel滤波器来检测边界
+    sobel_x = tf.image.sobel_edges(input_tensor)[:,:,:,0]
+    sobel_y = tf.image.sobel_edges(input_tensor)[:,:,:,1]
+    sobel_output = tf.math.abs(sobel_x) + tf.math.abs(sobel_y)
+
+    # 使用Conv层进一步提取边界特征
+    edge_features = tf.keras.layers.Conv2D(filters, kernel_size=(3,3), padding='same', activation='relu')(sobel_output)
+    edge_features = tf.keras.layers.BatchNormalization()(edge_features)
+    edge_features = tf.keras.layers.Conv2D(filters, kernel_size=(3,3), padding='same', activation='relu')(edge_features)
+    
+    # 添加残差连接
+    edge_residual = tf.keras.layers.Conv2D(filters, kernel_size=(1,1), padding='same', activation='relu')(sobel_output)
+    edge_features = tf.keras.layers.Add()([edge_features, edge_residual])
+    
+    # 使用一个1x1的Conv层来调整通道数量
+    edge_features = tf.keras.layers.Conv2D(filters, kernel_size=(1,1), padding='same', activation='relu')(edge_features)
+
+    # 合并原始特征和边界特征
+    merged = tf.keras.layers.Add()([input_tensor, edge_features])
+    
+    return merged
+
+
+
+    # 边界预测网络
+def boundary_prediction_subnetwork(x, filters):
+    # 一系列卷积层
+    x = tf.keras.layers.Conv2D(filters, (3, 3), padding="same", activation="relu")(x)
+    x = tf.keras.layers.Conv2D(filters//2, (3, 3), padding="same", activation="relu")(x)
+    # 输出边界概率图
+    boundary_map = tf.keras.layers.Conv2D(1, (1, 1), padding="same", activation="sigmoid")(x)
+    return boundary_map
 
 class MLPBlock(tf.keras.layers.Layer):
     # def __init__(self, units, activation="swish",name=None,trainable=True,dtype=None):
--- model.py
+++ model.py
@@ -1,15 +1,20 @@
 import tensorflow as tf
 from keras_cv_attention_models import caformer
 from layers.upsampling import decode
-from layers.convformer import convformer
+from layers.convformer import convformer as FBEM
 from layers.util_layers import merge, conv_bn_act
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D
 import tensorflow.keras.backend as K
+from tensorflow.keras.layers import DepthwiseConv2D, Conv2D, Multiply, GlobalAveragePooling2D, Reshape, Dense, Add
+from layers.AttentionBoundaryFusionBlock import AttentionBoundaryFusionBlock
+from tensorflow.keras.layers import BatchNormalization, Activation, Dropout
+
+
 
 def build_model(img_size = 256, num_classes = 1):
     backbone = caformer.CAFormerS18(input_shape=(256, 256, 3), pretrained="imagenet", num_classes = 0)
-
+    
     layer_names = ['stack4_block3_mlp_Dense_1', 'stack3_block9_mlp_Dense_1', 'stack2_block3_mlp_Dense_1', 'stack1_block3_mlp_Dense_1']
     layers = [backbone.get_layer(x).output for x in layer_names]
 
@@ -18,13 +23,24 @@ def build_model(img_size = 256, num_classes = 1):
 
     x = layers[0]
 
+        # 插入空间金字塔池化层
+    spp_output = spatial_pyramid_pooling(x, [1, 2, 4])  # 可以选择不同的池化尺度
+    x = tf.keras.layers.Concatenate(axis=channel_axis)([x, spp_output])
+
+
+
     upscale_feature = decode(x, scale = 4, filters = x.shape[channel_axis])
 
     for i, layer in enumerate(layers[1:]):
+    # for i, layer in enumerate(layers[1:]):
         
         x = decode(x, scale = 2, filters = layer.shape[channel_axis])
         
-        layer_fusion = convformer(layer, layer.shape[channel_axis])
+        # layer_fusion = FBEM(layer, layer.shape[channel_axis])
+        layer_fusion = FBEM(layer, layer.shape[channel_axis])
+        # attention_boundary_output = AttentionBoundaryFusionBlock(layer, layer.shape[channel_axis])
+        # layer_fusion = tf.keras.layers.Add()([layer_fusion_output, attention_boundary_output])
+
         
         ## Doing multi-level concatenation
         if (i%2 == 1):
@@ -32,6 +48,7 @@ def build_model(img_size = 256, num_classes = 1):
             x = tf.keras.layers.Add()([x, upscale_feature])
             x = tf.keras.layers.Conv2D(x.shape[channel_axis], (1, 1), activation = "relu", padding = "same")(x)
         
+        # x = adaptive_interaction_module(x)
         x = merge([x, layer_fusion], layer.shape[channel_axis])
         x = conv_bn_act(x, layer.shape[channel_axis], (1, 1))
 
@@ -41,6 +58,7 @@ def build_model(img_size = 256, num_classes = 1):
         
     filters = x.shape[channel_axis] //2
     upscale_feature = conv_bn_act(upscale_feature, filters, 1)
+    x = adaptive_interaction_module(x)
     x = decode(x, filters, 4)
     x = tf.keras.layers.Add()([x, upscale_feature])
     x = conv_bn_act(x, filters, 1)
@@ -48,3 +66,76 @@ def build_model(img_size = 256, num_classes = 1):
     model = Model(backbone.input, x)
 
     return model
+
+
+def spatial_pyramid_pooling(inputs, pool_list):
+    output = []
+    for pool_size in pool_list:
+        x = tf.keras.layers.AveragePooling2D(pool_size=pool_size)(inputs)
+        x = tf.keras.layers.Conv2D(512, 1, padding='same')(x)
+        x = tf.keras.layers.UpSampling2D(size=pool_size, interpolation='bilinear')(x)
+        output.append(x)
+    return tf.keras.layers.Concatenate(axis=-1)(output)
+
+
+def se_block(input_tensor, ratio=16):
+    """Squeeze and Excitation block."""
+    filters = input_tensor.shape[-1]
+    se_shape = (1, 1, filters)
+    
+    se = GlobalAveragePooling2D()(input_tensor)
+    se = Reshape(se_shape)(se)
+    se = Dense(filters // ratio, activation='relu')(se)
+    se = Dense(filters, activation='sigmoid')(se)
+
+    return Multiply()([input_tensor, se])
+
+def adaptive_interaction_module(inputs, dropout_rate=0.1):
+    """Adaptive Interaction Module (AIM)."""
+    # inputs = tf.keras.layers.LayerNormalization()(inputs)
+    # Depthwise Separable Convolution
+    x = DepthwiseConv2D(3, padding='same')(inputs)
+    x = Conv2D(inputs.shape[-1], 1, padding='same', activation='relu')(x)
+    
+    # Dilated Convolution with dilation rate 2
+    dilated_x = Conv2D(inputs.shape[-1], 3, dilation_rate=2, padding='same', activation='relu')(inputs)
+    
+    # Squeeze and Excitation block
+    se_x = se_block(Add()([x, dilated_x]))
+    
+    # se_x = Dropout(dropout_rate)(se_x)
+
+    return se_x
+
+
+# def swish(x):
+#     """Swish activation function"""
+#     return x * Activation('sigmoid')(x)
+
+
+# def adaptive_interaction_module(inputs, dropout_rate=0.2):
+#     """Adaptive Interaction Module (AIM)."""
+    
+#     # Depthwise Separable Convolution
+#     x = DepthwiseConv2D(3, padding='same')(inputs)
+#     # x = BatchNormalization()(x)
+#     # x = Activation(swish)(x)
+#     x = Activation('relu')(x)
+    
+#     x = Conv2D(inputs.shape[-1], 1, padding='same')(x)
+#     # x = BatchNormalization()(x)
+#     # x = Activation(swish)(x)
+#     x = Activation('relu')(x)
+#     # Dilated Convolution with dilation rate 2
+#     dilated_x = Conv2D(inputs.shape[-1], 3, dilation_rate=2, padding='same')(inputs)
+#     # dilated_x = BatchNormalization()(dilated_x)
+#     # dilated_x = Activation(swish)(dilated_x)
+#     dilated_x = Activation('relu')(dilated_x)
+#     # Merge and apply SE block
+#     merged_x = Add()([x, dilated_x])
+#     se_x = se_block(merged_x)
+    
+#     # Optional: Dropout for regularization
+#     se_x = Dropout(dropout_rate)(se_x)
+    
+#     return se_x
